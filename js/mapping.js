a73d037d990b9380e340dde61cc94ae3769368bc
davidxvuong <david.vuong@me.com>
2014-10-06 15:17:53 -0400

    Display map onto webpage and identify location

--- js/mapping.js
+++ js/mapping.js
@@ -1,5 +1,23 @@
-$(document).ready(function(){
-	var map = new GMap2(document.getElementById('map'));
-	var location = new GLatLng(43.655005, -79.389665);
+$(function(){
+	var map = new google.maps.Map2(document.getElementById('map'));
+	var location = new google.maps.LatLng(43.656018, -79.389793);
 	map.setCenter(location, 8);
+	
+	alert("test");
+	
+	var markers = [];
+	
+/* 	for (var i = 0; i < 10; i++)
+	{
+ */		var point = new google.maps.LatLng(43.656018, -79.389793); //new GLatLng(southWest.lat() + latSpan * Math.random(), southWest.lng() + lngSpan * Math.random());
+		var marker = new google.maps.Marker(point);
+		map.addOverlay(marker);
+		markers[i] = marker;
+	/* } */
+	
+	$(markers).each(function(i, marker){
+		google.maps.Event.addListener(marker, "click", function(){
+			map.panTo(marker.GetLatLng());
+		});
+	});
 });
