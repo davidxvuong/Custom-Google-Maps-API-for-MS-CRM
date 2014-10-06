169cb3dc969dac700d9e17063ba48fc0e74ddeab
davidxvuong <david.vuong@me.com>
2014-10-06 16:37:28 -0400

    Final changes
    - Google Maps is able to display onto screen
    - Markers are placed, but isn't visible on the map.

--- js/mapping.js
+++ js/mapping.js
@@ -1,19 +1,15 @@
-$(function(){
-	var map = new google.maps.Map2(document.getElementById('map'));
+$(document).ready(function(){
+	var map = new google.maps.Map(document.getElementById('map'));
 	var location = new google.maps.LatLng(43.656018, -79.389793);
 	map.setCenter(location, 8);
 	
-	alert("test");
-	
 	var markers = [];
 	
-/* 	for (var i = 0; i < 10; i++)
-	{
- */		var point = new google.maps.LatLng(43.656018, -79.389793); //new GLatLng(southWest.lat() + latSpan * Math.random(), southWest.lng() + lngSpan * Math.random());
-		var marker = new google.maps.Marker(point);
-		map.addOverlay(marker);
-		markers[i] = marker;
-	/* } */
+	var point = new google.maps.LatLng(43.703106, -79.290699);
+	var marker = new google.maps.Marker(point);
+	
+	marker.setMap(map);
+	markers[i] = marker;
 	
 	$(markers).each(function(i, marker){
 		google.maps.Event.addListener(marker, "click", function(){
