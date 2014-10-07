d5f0397049951c3dc41cfe382b1e52cb62e31008
davidxvuong <david.vuong@me.com>
2014-10-07 15:08:52 -0400

    added mapping code to display offices onto google maps

--- js/mapping.js
+++ js/mapping.js
@@ -1,14 +1,15 @@
-
+//global variable declarations
 var locations = [];
+var markers = [];
 
+//retrieve office location information when the webpage is loaded
 window.onload = function load(){
-	//retrieve data from xml locations fileCreatedDate
-	
 	$.ajax({
 		type: "GET",
 		url: "officeLocations.xml",
 		dataType: "xml",
 		success: function(xml){
+			console.log("Parsing XML...");
 			var i = 0;
 			$(xml).find('marker').each(function(){
 				var name = $(this).attr('name');
@@ -20,17 +21,55 @@ window.onload = function load(){
 				var city = $(this).attr('city');
 				var state = $(this).attr('state');
 				var postal = $(this).attr('postal');
-				
-				locations[i] = new Array(name, lat, lng, type, address, address2, city, state, postal);
-				alert(name);
+				locations[i] = new Array(name, lat, lng, address, type, address2, city, state, postal);
+
 				i++;
 			});
+			
+			console.log("Parsing completed. Length of Array: " + locations.length);
+			initialize();
 		}
 	});
+	
 }
+//sets up map, placing location markers onto the map
+function initialize() {
+	var myLatlng = new google.maps.LatLng(43.655583, -79.389665);
+	var mapOptions = {
+        center:myLatlng,
+        zoom:15,
+        disableDefaultUI:true,
+        mapTypeId:google.maps.MapTypeId.ROADMAP
+    }
+	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+	var markerImage = 'images/workoffice.png';
+	
+	for (var j = 0; j <= locations.length; j++){
+		markerFactory(locations[j], map, markerImage);
+	}
+ }
+
+ function markerFactory(information, thisMap, image){
+	var latLng = new google.maps.LatLng(information[1], information[2]);
+	var marker = new google.maps.Marker({
+		position: latLng,
+		map: thisMap,
+		icon: image,
+	});
+ }
+//.maps.event.addDomListener(window, 'load', initialize);
 
 
-//google.maps.event.addDomListener(window, 'load', initialize);
+/* function markerFactory(var information, var thisMap, image){
+		var latLng = new google.maps.LatLng(information[1], information[2]);
+		
+		var marker = new google.maps.Marker({
+			position: latLng,
+			map: thisMap,
+			icon: image
+		});
+}
+ */
 
 /*
 
