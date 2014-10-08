600acde10e16de25200764b6395d9c8192b058fb
davidxvuong <david.vuong@me.com>
2014-10-08 11:31:37 -0400

    Implemented dialog box to pop office location records

--- js/mapping.js
+++ js/mapping.js
@@ -10,7 +10,6 @@ window.onload = function load(){
 		url: "officeLocations.xml",
 		dataType: "xml",
 		success: function(xml){
-			console.log("Parsing XML...");
 			var i = 0;
 			$(xml).find('marker').each(function(){
 				var name = $(this).attr('name');
@@ -27,7 +26,6 @@ window.onload = function load(){
 				i++;
 			});
 			
-			console.log("Parsing completed. Length of Array: " + locations.length);
 			initialize();
 		}
 	});
@@ -46,40 +44,56 @@ function initialize() {
 	map = new google.maps.Map(document.getElementById("map"), mapOptions);
 	var markerImage = 'images/pinother.png';
 	for (var j = 0; j < locations.length; j++){
-		markers[j] = markerFactory(locations[j][1], locations[j][2], map, markerImage);
+		markers[j] = markerFactory(locations[j], map, markerImage);
 	}
-	console.log("MS Office mapping complete. Length of markers array: " + markers.length);
- }
+}
 
  //A function that creates a marker on the map
- function markerFactory(lat, lng, thisMap, image){
-	var latLng = new google.maps.LatLng(lat, lng);
+ function markerFactory(info, thisMap, image){
+	var information = new google.maps.InfoWindow();
+	var latLng = new google.maps.LatLng(info[1], info[2]);
 	var marker = new google.maps.Marker({
 		position: latLng,
 		map: thisMap,
 		icon: image,
+		title: info[0],
 	});
 	
-	google.maps.event.addListener(marker, "click", function(){
-		thisMap.panTo(marker.getPosition());
+	google.maps.event.addListener(marker, "click", function(){		
+		var locationInfo;
+		map.panTo(marker.getPosition());
+		
+		locationInfo = "<div><h3>" + info[0] + "</h3><p>" + info[3] + ", " + info[5] + "<br>" + info[6] + ", " + info[7] + "<br>" + info[8] + "</p></div>";
+		
+		information.setContent(locationInfo);
+		information.open(map, marker);
 	});
 	
 	return marker;
  }
- 
+  
  //CRM inputs address
  function submitAddress() {
 	var address = document.getElementById('input').value; //to be replaced by CRM information
 	var geocoder = new google.maps.Geocoder();
-	
+	//locations[i] = new Array(name, lat, lng, address, type, address2, city, state, postal);
+
 	geocoder.geocode({'address': address}, function(results, status){
 		if (status == google.maps.GeocoderStatus.OK) {
+			var name = results[0].address_components[0].long_name + " " + results[0].address_components[1].long_name;
 			var lat = results[0].geometry.location.k;
 			var lng = results[0].geometry.location.B;
+			var type = "Client Location";
+			var city = results[0].address_components[2].long_name;
+			var state = results[0].address_components[4].long_name;
+			var postal = results[0].address_components[6].long_name;
 			var markerImage = 'images/home.png';
+			
+			var passOver = [name, lat, lng, name, type, "", city, state, postal];
 			map.setCenter(new google.maps.LatLng(lat, lng));
 			map.setZoom(12);
-			markerFactory(lat, lng, map, markerImage);
+			markerFactory(passOver, map, markerImage);
+			console.log(results);
 		}
 		else {
 			alert("Error: " + status);
