7bb2d98036682e405ba467042cfd18a3f8d04ae2
davidxvuong <david.vuong@me.com>
2014-10-09 10:34:44 -0400

    added ternary expression to determine marker information type

--- js/mapping.js
+++ js/mapping.js
@@ -1,6 +1,7 @@
 //global variable declarations
 var information = new google.maps.InfoWindow();
 var locations = [];
+var userLocation;
 var markers = [];
 var map;
 //retrieve office location information when the webpage is loaded
@@ -44,12 +45,12 @@ function initialize() {
 	map = new google.maps.Map(document.getElementById("map"), mapOptions);
 	var markerImage = 'images/pinother.png';
 	for (var j = 0; j < locations.length; j++){
-		markers[j] = markerFactory(locations[j], map, markerImage);
+		markers[j] = markerFactory(locations[j], map, markerImage, true);
 	}
 }
 
  //A function that creates a marker on the map
- function markerFactory(info, thisMap, image){
+ function markerFactory(info, thisMap, image, isXml){
 	var latLng = new google.maps.LatLng(info[1], info[2]);
 	var marker = new google.maps.Marker({
 		position: latLng,
@@ -62,7 +63,8 @@ function initialize() {
 		var locationInfo;
 		map.panTo(marker.getPosition());
 		
-		locationInfo = "<div><h3>" + info[0] + "</h3><p>" + info[3] + ", " + info[5] + "<br>" + info[6] + ", " + info[7] + "<br>" + info[8] + "</p></div>";
+		locationInfo = (isXml == true) ? ("<div><h3>" + info[0] + "</h3><p>" + info[3] + ", " + info[5] + "<br>" + info[6] + ", " + info[7] + "<br>" + info[8] + "</p></div>") : 
+			"<div><h3>" + info[0] + "</h3></div>";
 		information.setContent(locationInfo);
 		information.open(map, marker);
 	});
@@ -72,24 +74,22 @@ function initialize() {
   
  //CRM inputs address
  function submitAddress() {
-	var address = document.getElementById('input').value; //to be replaced with CRM information
+	var address = document.getElementById('input').value; //to be replaced with CRM information. Must contain address, city, and province
 	var geocoder = new google.maps.Geocoder();
 
 	geocoder.geocode({'address': address}, function(results, status){
 		if (status == google.maps.GeocoderStatus.OK) {
-			var name = results[0].address_components[0].long_name + " " + results[0].address_components[1].long_name;
+			var name = results[0].formatted_address;
 			var lat = results[0].geometry.location.k;
 			var lng = results[0].geometry.location.B;
-			var type = "Client Location";
-			var city = results[0].address_components[2].long_name;
-			var state = results[0].address_components[4].long_name;
-			var postal = results[0].address_components[6].long_name;
 			var markerImage = 'images/home.png';
-
-			var passOver = [name, lat, lng, name, type, "", city, state, postal];
+			
+			console.log(name)
+			
+			var passOver = [name, lat, lng];
 			map.setCenter(new google.maps.LatLng(lat, lng));
 			map.setZoom(12);
-			markerFactory(passOver, map, markerImage);
+			userLocation = markerFactory(passOver, map, markerImage, false);
 		}
 		else {
 			alert("Error: " + status);
